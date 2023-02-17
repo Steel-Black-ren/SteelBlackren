01275ebfea59e193058e8c38dcafb0eb51662f9c
超级人黑 <3547830398@qq.com>
2023-02-17 09:18:35 +0800

    版本1.0

--- testOne/src/test/java/testOne.java
+++ testOne/src/test/java/testOne.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class testOne {
+import org.junit.jupiter.api.Test;
+
+public class testOne {
+
+
+    @Test
+    public void testOne(){
+        if(1==1){
+            System.out.printf("HELLO！WORD！");
+        }else{
+            System.out.printf("我爱学习！");
+        }
+    }
 }
